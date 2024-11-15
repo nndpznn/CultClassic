d3877e789b5b397dc71319d8b2074e17675e16f8
Nolan Nguyen! <nolan2087@gmail.com>
2024-11-16 01:24:28 +0900

    moving other testing resources from our online data out of the way, into another folder | getting started on some data pre-processing

--- data/datasets.py
+++ data/datasets.py
@@ -1,4 +1,18 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split as tts
 
-movieData = pd.read_csv("raw/ml-100k/u.data", sep="\t")
-print(movieData.head(10))
+data = pd.read_csv("raw/ml-100k/u.data", sep="\t")
+# print(data.head(10))
+
+# Casting data to correct types for use 
+data["userID"] = data["userID"].astype(int)
+data["movieID"] = data["movieID"].astype(int)
+data["rating"] = data["rating"].astype(float)
+
+# data.head(), data.info()
+
+# Using train_test_split() to randomly partition 1/5 of the data for testing.
+trainingData, testingData = tts(data, test_size=0.2, random_state=None)
+
+
+
